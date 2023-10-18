c763a16b3a4999f33ca029206ab91edd0930f0b7
Nadine <nadineatoccsaortiz@gmail.com>
2023-10-18 11:47:03 -0500

    class producto usando list, filter, objetos, etc

--- poo_lp/ejercicio_avanzado_producto.py
+++ poo_lp/ejercicio_avanzado_producto.py
@@ -42,6 +42,7 @@ productos=[
     {
         'id':1,
         'nombre':'arroz',
+        'codigo':'2023-A',
         'descripcion':'costeño costal x 100 kg',
         'stock':5,
         'unidad':'costales',
@@ -57,7 +58,8 @@ class Producto:
 
 
     ## atribustos de instancia
-    def __init__(self, nombre, descripcion, stock, unidad, precio, moneda='soles',):
+    def __init__(self, codigo, nombre, descripcion, stock, unidad, precio, moneda='soles',):
+        self.codigo=codigo
         self.nombre=nombre
         self.descripcion=descripcion
         self.stock=stock
@@ -69,7 +71,7 @@ class Producto:
     ## creacion de metodos
     def mostrar_productos(self):
         mensaje=f"""
-        tienes {len(productos)}
+        tienes {len(productos)} productos
         los productos son:
         {productos}
         """
@@ -82,6 +84,7 @@ class Producto:
         nuevo_id=len(productos)+1
         producto_nuevo={
             "id":nuevo_id,
+            'codigo':self.codigo,
             "nombre":self.nombre,
             'descripcion':self.descripcion,
             'stock':self.stock,
@@ -97,17 +100,28 @@ class Producto:
         return f"el siguiente producto fue eliminado {producto_eliminar}"
 
     def actualizar_producto(self, id, clave, valor):
-        productos[id-1][clave]=valor
-        pass
-    
+        ol=valor
+        producto_actualizar=list(filter(lambda obj:obj[clave]==id,productos))[0].update({clave:valor}) ## donde dice objet le puedo poner cualquier nombre, 
+        # no necesariamente se tiene que llamar objet, es como una nueva variable donde se captura o guarda los prdoductos filtrados
+        
+        # productos[id-1][clave]=valor
+        return 'se actualizo'
 
-prod=Producto('aceite', 'extra virgen',2, 'botella x litro', 12.5)
+## metodod funcional filter, filtra o retorna un lemento que sea true de una lista
+## funciones autoejecutadas o anonimas en puthon, se les conoce como funciones lambda
+## lambda es una funcion anonima
+## su uso o estructura
+## lambda a,b : return a+b, el nombre de la funcion es lambda, los parametros a,b y el resultado return
+# es una funcion en una sola linea, se va ejecutar automaticamente sin llamarla, 
+## si quiero ejecutar esa funcion suma=lambda a,b : return a+b y poder llamar a la variable 
+## suma(3,4) ## muestra 7
+## no se puede usar en una clase o metodos de un objeto, solo para basico
+## lista funcion para crear lista m=list('hola) me devuelve ['h','o','l','a']
+## averiguar sobre programacion funcional en python
+
+prod=Producto('2023-b','aceite', 'extra virgen',2, 'botella x litro', 12.5)
 print(prod.registrar_producto())
 print(prod.mostrar_productos())
-print(prod.mostrar_producto(1))
-print(prod.eliminar_producto(2))
+print(prod.actualizar_producto(125,'precio','130'))
 print(prod.mostrar_productos())
-print(prod.actualizar_producto(1,clave='nombre',valor='aceite'))
-print(prod.mostrar_productos())
-
-
+# print(a.actualizar_alumno(1,clave='edad',valor='aceite'))
