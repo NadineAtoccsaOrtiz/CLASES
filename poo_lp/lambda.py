ed2fdf15ab51852bc710758957eb2fd978d5e3c6
Nadine <nadineatoccsaortiz@gmail.com>
2023-10-18 12:55:48 -0500

    tarea

--- poo_lp/lambda.py
+++ poo_lp/lambda.py
@@ -34,7 +34,7 @@ lista_alumnos=[
         'nombre':'del mar',
         'edad':30,
         'hobbie':'saltar',
-        'flaquita':'msami'
+        'flaquita':'sami'
     },
 
     {
@@ -61,3 +61,46 @@ lista_alumnos=[
 print(f'todos mis alumnos {lista_alumnos}')
 fans_melody=list(filter(lambda par:par['flaquita']=='melody',lista_alumnos))
 print(f'los que quieren con melody {fans_melody}')
+
+
+
+fans_melody=list(filter(lambda par:par['edad']>49,lista_alumnos))
+print(f'los que quieren con melody {fans_melody}')
+
+## averiguar como hacer para que en mi objeto solo me muestre el nombre y flaquita 
+
+## map 
+
+nuevo_objeto=list(map(lambda par:{'nombre_alumno': par ['nombre'],'germita':par ['flaquita']}, lista_alumnos))
+print(nuevo_objeto)
+
+
+## tarea
+# 1. crear una lista con 10 objetos que contengan los datos de las tiendas comerciales 
+#  ejemplo 
+tiendas = [
+    {
+        'nombre':'el pichito',
+        'categoria':['abarrotes', 'bodega'], # va ser un array, puede devolver dos funciones o uno o tres
+        'horario_atencion':{
+            'dia': 7am-12pm',
+            'tarde': 2pm - 8pm
+                        },
+        'gerente': ' nadine' # va ser un objeto y va devolver dos 
+    }
+]
+## observacion: las categorias seran 4
+# abarrotes 
+# farmacia
+# bodega
+## observacion: los gerentes solo popdran ser los siguientes: 
+## edwin
+# china
+# crhistian
+# nadine
+
+# realizar lo siguientes
+# 1. crear unna clase que tenga los siguientes metodos o casos de uso
+# - crear un metodo  que me filtre las tiendas que tienen cada gerente pasandole el parametro de cada uno: por ejemplo si digo edwin me muestra las tiendas de edwin
+# - crear un metodo que muestre los negocios que tienen mas de dos categorias
+# - crear un metodo que me muestre solo el nombre de las tiendas
