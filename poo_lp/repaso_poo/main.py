1184d14904a7c6c4a831bfea3977d7d9704d0209
Nadine <nadineatoccsaortiz@gmail.com>
2023-11-07 12:08:46 -0500

    clase rich entorno virtual

---
+++ poo_lp/repaso_poo/main.py
@@ -0,0 +1,28 @@
+from rich import print 
+from rich.markdown import Markdown
+edad=12
+respuesta='[bold blue] es mayor de edad' if edad >17 else '[italic underline]es menor de edad'
+
+texto='''
+# titulo
+parrafo
+```
+bash
+git commit -m "cuerpo del commit"
+```
+*lista
+
+*lista
+> info valiosa
+{}
+
+'''.format(respuesta)
+
+mostrar_markdown=Markdown(texto)
+print(mostrar_markdown)
+
+
+
+
+
+## cambiar python que tenemos dentro de script en el entorno, clic enn version python, seleccionar interprete
